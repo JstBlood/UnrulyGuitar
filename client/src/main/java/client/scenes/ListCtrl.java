8f2f7a1d9c5e4d52d7117b6a8cd3da9e637c886e
Bogdan Micu <b.a.micu@student.tudelft.nl>
2023-03-10 17:35:44 +0100

    added remove button and changed the tasks from TitledPane to String for now

--- client/src/main/java/client/scenes/ListCtrl.java
+++ client/src/main/java/client/scenes/ListCtrl.java
@@ -3,21 +3,30 @@ package client.scenes;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 
 public class ListCtrl {
     @FXML
-    public ListView<TitledPane> listView;
+    public ListView<String> listView;
     private int i=0;
     @Inject
-    public ListCtrl(ListView<TitledPane> listView){
+    public ListCtrl(ListView<String> listView){
         this.listView=listView;
     }
-    public void click(){
-        listView.getItems().add(new TitledPane("Dummy Card "+i,new Label("The subtasks of the card should be here")));
+    @FXML
+    public void addTask(){
+        /*
+        new TitledPane("Dummy Card "+i,new Label("The subtasks of the card should be here"))
+        We could display the tasks like this, but the selection mode works poorly with it
+        */
+        listView.getItems().add("Dummy Card"+i);
         i++;
+    }
+    @FXML
+    public void removeTask(){
+        int id=listView.getSelectionModel().getSelectedIndex();
+        listView.getItems().remove(id);
     }
 }
