500f9a2c45b32c9b7b65936d428d2ac95f02120f
Madeline Sebastian <m.e.sebastian@student.tudelft.nl>
2023-03-15 12:25:01 +0000

    removed some unnecessary stuff 2

--- server/src/main/java/server/api/CardController.java
+++ server/src/main/java/server/api/CardController.java
@@ -1,9 +1,5 @@
 package server.api;
 
-import commons.Board;
-import commons.Card;
-import commons.CardList;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import server.database.BoardRepository;
