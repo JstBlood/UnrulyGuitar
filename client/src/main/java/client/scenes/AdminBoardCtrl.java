30d94db6d83168a4ee1f14b7bbb40b6d6c233938
Mikołaj Gazeel <m.j.gazeel@student.tudelft.nl>
2023-03-29 02:25:12 +0200

    implement removing boards from the overwiew for admins

---
+++ client/src/main/java/client/scenes/AdminBoardCtrl.java
@@ -0,0 +1,43 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import commons.Board;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * This class is the controller of the AddCardList scene,
+ * where the user can create a new CardList for the current board.
+ */
+
+public class AdminBoardCtrl implements Initializable{
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+    private Board b;
+
+    @FXML
+    private Label bid;
+
+    @Inject
+    public AdminBoardCtrl(ServerUtils server, MainCtrl mainCtrl, Board b) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+        this.b = b;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.bid.setText(b.key);
+    }
+
+    @FXML
+    public void delete() {
+        server.deleteBoard(bid.getText());
+        mainCtrl.showBoards();
+    }
+}
