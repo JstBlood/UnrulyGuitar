115fea25cd474c1bad08a49ad8be42a051f6d290
Matiss Berzins <M.Berzins-1@student.tudelft.nl>
2023-04-01 02:56:03 +0200

    TaskService improvements

--- server/src/main/java/server/api/TaskController.java
+++ server/src/main/java/server/api/TaskController.java
@@ -33,17 +33,4 @@ public class TaskController {
         return ResponseEntity.status(taskService.update(id, component, newValue, username, password)).build();
     }
 
-    @PutMapping("/{id}/title")
-    public ResponseEntity<?> update(@PathVariable long id,
-                                    @RequestBody String newValue, @PathVariable String username,
-                                    @PathVariable(required = false) String password) {
-        return ResponseEntity.status(taskService.update(id, "title", newValue, username, password)).build();
-    }
-
-    @PutMapping("/{id}/isDone")
-    public ResponseEntity<?> update(@PathVariable long id,
-                                    @RequestBody boolean newValue, @PathVariable String username,
-                                    @PathVariable(required = false) String password) {
-        return ResponseEntity.status(taskService.update(id, "isDone", newValue, username, password)).build();
-    }
 }
